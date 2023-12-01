20670b4eeb12c81bad37d7e940c50d80d1a6b144
franktraining <musabaytechcorp@gmail.com>
2023-11-30 20:48:52 -0500

    upadted hello world class three times

--- LearingGitHub1/src/main/java/com/git/learning/practice/Practice.java
+++ LearingGitHub1/src/main/java/com/git/learning/practice/Practice.java
@@ -6,6 +6,9 @@ public class Practice {
 	public static void main(String[] args) {
 		
 		System.out.println("Hello World !");
+		System.out.println("Hello hELLO World !");
+		System.out.println("World !");
+		System.out.println("i hate java!");
 	}
 	
 }
